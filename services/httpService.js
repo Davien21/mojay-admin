1218efd08efa3884b4247ce0122b2b540c6782d2
davien21 <chidiebereekennia@gmail.com>
2021-11-17 03:17:37 +0100

    update size formatter

--- services/httpService.js
+++ services/httpService.js
@@ -12,7 +12,7 @@ axios.interceptors.response.use(null, (error) => {
     // toast.error("An unexpected error occurred.");
   }
 
-  console.log(error.response)
+  console.log(error.response);
   // if (expectedError) console.log(error);
 
   return Promise.reject(error);
--- utils/format-size.js
+++ utils/format-size.js
@@ -1,6 +1,7 @@
 function formatSize(size) {
-  if (size < 1000) return `${size} bytes`
-  if (size >= 1000) return `${size / 1000} kb`
+  if (size < 1000) return `${size} bytes`;
+  if (size >= 1000 && size < 1000000) return `${(size / 1000).toFixed(2)} KB`;
+  if (size >= 1000000) return `${(size / 1000000).toFixed(2)} MB`;
 }
 
 export { formatSize };
